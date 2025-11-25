5447824690596b4c19717046805739ddf06b9524
Sanjiti <sanjitishergill@gmail.com>
2025-11-25 14:19:31 +0530

    Create static_data_members.cpp

---
+++ static members/static_data_members.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    static int count;   // static data member
+
+public:
+    Student() {
+        count++;
+    }
+
+    void showCount() {
+        cout << "Total Students: " << count << endl;
+    }
+};
+
+// Definition of static member (outside class)
+int Student::count = 0;
+
+int main() {
+    Student s1, s2, s3;
+    s1.showCount();   // prints 3
+    return 0;
+}
